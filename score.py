f08e44c0bc19b1b2db1165abfcea20267edb77f3
Amine AKROUT <akrout.med.amine@gmail.com>
2021-12-13 17:14:57 +0100

    deploy model

---
+++ score.py
@@ -0,0 +1,25 @@
+
+import json
+import os
+import tensorflow as tf
+import numpy as np
+
+from azureml.core.model import Model
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+
+def init():
+    global model
+    model_path = Model.get_model_path('diamond_model')
+    model = tf.keras.models.load_model(model_path)
+
+
+def run(data):
+    test = json.loads(data)
+    print(f"received data {test}")
+    input_dict = {name: tf.convert_to_tensor([value]) for name, value in test.items()}
+    predictions = model.predict(input_dict)
+    predicted = predictions[0]
+    result = str(round(predicted[0],2))
+    return result
